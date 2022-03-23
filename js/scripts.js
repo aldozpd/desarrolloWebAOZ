7305f3a6c03671246d6a916732fe9234d112d2fe
Aldo Oaxaca <420086112@pcpuma.acatlan.unam.mx>
2022-03-22 23:22:55 -0600

    Primer practica js
    Se utilizan funciones en js para cambiar el atributo "src" de una etiqueta img para actualizar la imagen mostrada

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function statusConnected(){
+    var statusImg = document.getElementById("statusImg");
+    statusImg.setAttribute("src","media/img/connected.png");
+}
+
+function statusDisconnected(){
+    var statusImg = document.getElementById("statusImg");
+    statusImg.setAttribute("src","media/img/disconnected.jpg");
+}
